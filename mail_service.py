ff81f836d0074bd8fd7e2466907363ae60a4e962
User <User@DESKTOP-8TVF7S9>
2023-01-18 19:29:31 +0300

    Поправил response

--- mail_service.py
+++ mail_service.py
@@ -10,18 +10,28 @@ mail_password = 'ffjepfbqifozdini'
 def send_response(name, email, tel, req_type, comment):
     sender_email = "yui678mf@gmail.com"
     receiver_email = "yui678m@mail.ru"
-    msg = MIMEMultipart()
+    msg = MIMEMultipart("alternative")
     msg['Subject'] = 'Response'
     msg['From'] = sender_email
     msg['To'] = receiver_email
+
+    body_text = """
+    Name: {name}   \r\n
+    Email: {email}   \r\n
+    Telephone: {tel}   \r\n
+    Request type: {req_type}   \r\n
+    Comment: {comment}   \r\n
+    """.format(name=name, email=email, tel=tel, req_type=req_type, comment=comment)
+
     env = Environment(
         loader=FileSystemLoader('templates'),
         autoescape=select_autoescape(['html', 'xml'])
     )
     template = env.get_template('response_form.html')
-    body = template.render(name=name, email=email, tel=tel, req_type=req_type, comment=comment)
+    body_html = template.render(name=name, email=email, tel=tel, req_type=req_type, comment=comment)
+    msg.attach(MIMEText(body_text, 'plain', 'utf-8'))
+    msg.attach(MIMEText(body_html, 'html', 'utf-8'))
 
-    msg.attach(MIMEText(body, 'html', 'utf-8'))
 
 
 
